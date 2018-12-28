a1a97d1ffe064af77c1a33c2bfa5b79beaac22eb
Thomas Merlin <dev.thomasmerlin@gmail.com>
2018-12-28 15:14:35 +0100

    Creating the first command concerning the database

--- src/BackendGeneratorBundle.php
+++ src/BackendGeneratorBundle.php
@@ -4,6 +4,10 @@ namespace Floaush\Bundle\BackendGeneratorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class BackendGeneratorBundle
+ * @package Floaush\Bundle\BackendGeneratorBundle
+ */
 class BackendGeneratorBundle extends Bundle
 {
 }
